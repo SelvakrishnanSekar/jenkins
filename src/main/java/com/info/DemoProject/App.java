15fdf6e1b8636508313cd46f48f5228fc404ac53
Selvakrishnan Sekar <selvakrishnansekar@gmail.com>
2020-01-03 16:19:49 +0530

    Adding DemoProject modify

--- src/main/java/com/info/DemoProject/App.java
+++ src/main/java/com/info/DemoProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, This is a World! test jenkins" );
+        System.out.println( "geetham software" );
     }
 }
